38b99a3fc9d62fcf5bd020059e632df63f2cbb2c
VictorCstr <victorcastro.dev@hotmail.com>
2021-12-15 19:23:44 -0300

    Add funcionalidade de deletar, array global e correcao de detalhes

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const listaServicos = [];
+
 //Preview da imagem no formulario
 var preview = document.getElementById('previewimage');
 preview.style.display= "none";
@@ -8,10 +10,18 @@ var carregarImagem = function(event) {
 
 //Função que ativa ao enviar o formulário adicionando um serviço novo
  function adicionarServico(){
+   //Declarando os valores de cada variavel de acordo com o que o usuario preencher
     let nome = document.getElementById('servico').value;
     let arquivo = document.getElementById('arquivo').value;
     let descricao = document.getElementById('descricao').value;
     let tabela = document.getElementById('minha-tabela');
+    let servico ={
+      nome: nome,
+      arquivo: arquivo,
+      descricao: descricao
+    }
+    //Adicionando os dados a lista geral
+    listaServicos.push(servico)
 
     //Condicional para impedir de criar um serviço em branco
     if(nome.length>0 && descricao.length>2 && arquivo.length>5){
@@ -26,7 +36,7 @@ var carregarImagem = function(event) {
 
     //Criação do Botão de editar e remover para inserir na célula
     let btnEditar = '<button class="btn btn-secondary m-1">editar</button>'
-    let btnRemover = '<button class="btn btn-danger m-1">excluir</button>'
+    let btnRemover = '<button class="btn btn-danger m-1" onclick="removerServico(this)">excluir</button>'
 
     //Aqui estamos fazendo a inserção dos dados nas celulas novas da tabela
     cell1.innerHTML = nome
@@ -38,8 +48,16 @@ var carregarImagem = function(event) {
     document.getElementById('servico').value = "";
     document.getElementById('arquivo').value = "";
     document.getElementById('descricao').value = "";
-
+    preview.src= ""
+    preview.style.display="none"
 
 } else{
   window.alert('Estão faltando dados para inserir o serviço novo!')
 }}
+
+
+function removerServico(r){
+  var i = r.parentNode.parentNode.rowIndex;
+  let tabela = document.getElementById('minha-tabela');
+  tabela.deleteRow(i)
+}
